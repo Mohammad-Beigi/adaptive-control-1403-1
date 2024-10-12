caa656b5d8ec9bcee2a3d6337240a59ef205964f
Beigi <m.mohammadbeigi28@gmail.com>
2024-10-12 14:56:18 +0330

    plot loss function for differenct model with bar.

--- sec2_parameter_estimation/example1.m
+++ sec2_parameter_estimation/example1.m
@@ -1,30 +1,43 @@
 %% Example 2.1 Least-square estimation of static system
+clear, clc
 
 U = [0, 1, 2, 3, 4, 5, 6]';
 Y = [1, 2, 2.2, 3.5, 4.6, 6.1, 7.8]';
 
 num_data = length(U);
 poly_order = [0 1 2 3];
+Theta = cell(4,1);
+V = zeros(4,1);
 
-for m = 1:4
+%%
+for i = 1:4
     % Compute Phi
-    Phi = ones(num_data, poly_order(m)+1);
-    for i = 1:poly_order(m)
-        Phi(:,i+1) = Phi(:,i) .* U;
+    Phi = ones(num_data, poly_order(i)+1);
+    for j = 1:poly_order(i)
+        Phi(:,j+1) = Phi(:,j) .* U;
     end
 
     % Estimate Parameter
-    Theta = (Phi' * Phi) \ Phi' * Y;
+    theta = (Phi' * Phi) \ Phi' * Y;
+    Theta{i} = theta;
 
     % loss function
-    V = norm(Y - Phi * Theta, 2);
+    e = Y - Phi * theta;
+    V(i) = e' * e;
 
-    subplot(2,2,m)
+    % Plot
+    subplot(2,2,i)
     plot(U, Y, 'bo', "LineWidth", 2), hold on
-    plot(U, Phi * Theta, 'r', "LineWidth", 2), hold off
+    plot(U, Phi * theta, 'r', "LineWidth", 2), hold off
     grid on
-    title("Order = " + poly_order(m) + ",  loss = " + V)
+    title("Order = " + poly_order(i) + ",  loss = " + V(i))
     xlabel("Input")
     ylabel("Output")
-
 end
+
+%% Loss
+figure
+bar(poly_order,V)
+title("Loss Value for Different Model")
+xlabel("polynomial order")
+ylabel("squared sum of error")
