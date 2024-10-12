b8de67da8c6abe0a2eecb128c51f6a042fa2e8ac
Beigi <m.mohammadbeigi28@gmail.com>
2024-10-12 13:06:20 +0330

    Estimate polynomial model parameter from input and output data using least square method, and plot the real data and estimated one.

--- sec2_parameter_estimation/example1.m
+++ sec2_parameter_estimation/example1.m
@@ -1,4 +1,30 @@
 %% Example 2.1 Least-square estimation of static system
 
-input = [0, 1, 2, 3, 4, 5, 6]';
-output = [1, 2, 2.2, 3.5, 4.6, 6.1, 7.8]';
+U = [0, 1, 2, 3, 4, 5, 6]';
+Y = [1, 2, 2.2, 3.5, 4.6, 6.1, 7.8]';
+
+num_data = length(U);
+poly_order = [0 1 2 3];
+
+for m = 1:4
+    % Compute Phi
+    Phi = ones(num_data, poly_order(m)+1);
+    for i = 1:poly_order(m)
+        Phi(:,i+1) = Phi(:,i) .* U;
+    end
+
+    % Estimate Parameter
+    Theta = (Phi' * Phi) \ Phi' * Y;
+
+    % loss function
+    V = norm(Y - Phi * Theta, 2);
+
+    subplot(2,2,m)
+    plot(U, Y, 'bo', "LineWidth", 2), hold on
+    plot(U, Phi * Theta, 'r', "LineWidth", 2), hold off
+    grid on
+    title("Order = " + poly_order(m) + ",  loss = " + V)
+    xlabel("Input")
+    ylabel("Output")
+
+end
