2cacc3c098d67261d0f57083d4d6f36070da41db
Beigi <m.mohammadbeigi28@gmail.com>
2024-10-20 11:23:10 +0330

    Add some parameters. A, B, and C are characteristic polynomials of y, u, and e. start_time, stop_time, and sample_time are for simulink setting.

--- sec2_parameter_estimation/example12.m
+++ sec2_parameter_estimation/example12.m
@@ -1,14 +1,22 @@
 %% Simulation of Recuresive Estimation
 start_time = 0;
-stop_time = 100;
+stop_time = 1000;
 sample_time = 1;
 
 % y(t) + a y(t-1) = b u(t-1) + e(t) + c e(t-1)
 a = -0.8;
 b = 0.5;
+c = 0;
+
+% A(q) y(t) = B(q) u(t) + C(1) e(t)
+A = [1, a];
+B = b;
+C = [1, c];
+
+% noise parameter
 std_e = 0.5; % standard deviation of e(t)
 mean_e = 0;
 seed_e = 12;
-c = 0;
+
 P0 = 100;
 theta0 = 0;
