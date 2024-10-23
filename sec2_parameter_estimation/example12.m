49ab43d82d5e9ef3021630af04403359a29c1239
Beigi <m.mohammadbeigi28@gmail.com>
2024-10-23 22:41:19 +0330

    plot simulation result.

--- sec2_parameter_estimation/example12.m
+++ sec2_parameter_estimation/example12.m
@@ -1,22 +1,46 @@
 %% Simulation of Recuresive Estimation
+clear, clc
+%% Model Settings > Solver
 start_time = 0;
 stop_time = 1000;
 sample_time = 1;
 
+%% Parameters
+% Model
 % y(t) + a y(t-1) = b u(t-1) + e(t) + c e(t-1)
 a = -0.8;
 b = 0.5;
-c = 0;
+c = -0.5;
 
-% A(q) y(t) = B(q) u(t) + C(1) e(t)
+% A(q) y(t) = B(q) u(t) + C(q) e(t)
 A = [1, a];
 B = b;
 C = [1, c];
 
-% noise parameter
+% Noise
 std_e = 0.5;
 mean_e = 0;
 seed_e = 6;
 
-P0 = 100 * eye(2);
-theta0 = zeros(2,1);
+% Initial
+P0 = 100 * eye(3);
+theta0 = zeros(3,1);
+%% Simulation
+% input_type: {(1, 'unit pulse'), (2, 'periodic pulse')}
+input_type = 1;
+% regressor_type: {(1, 'rls'), (2, 'els')}
+regressor_type = 1;
+data = sim("recursive_estimation.slx");
+%% Plot
+line_width = 3;
+
+figure
+plot(data.theta.Time, data.theta.Data(:,1), 'b-', 'LineWidth', line_width)
+hold on, grid on
+plot([start_time stop_time], [a a], 'b--', 'LineWidth', line_width)
+plot(data.theta.Time, data.theta.Data(:,2), 'r-','LineWidth',line_width)
+plot([start_time stop_time], [b b], 'r--', 'LineWidth', line_width)
+plot(data.theta.Time, data.theta.Data(:,3), 'g-','LineWidth',line_width)
+plot([start_time stop_time], [c c], 'g--', 'LineWidth', line_width)
+xlabel('Sample')
+legend({'$\hat{a}$','$a$','$\hat{b}$','$b$','$\hat{c}$','$c$'},'Interpreter','latex')
