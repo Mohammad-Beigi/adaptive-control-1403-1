ca035bcb7b21bf91ee97fce3f841a4e9f317404c
Beigi <m.mohammadbeigi28@gmail.com>
2024-10-31 20:26:17 +0330

    complete plotting code.

--- sec2_parameter_estimation/example14.m
+++ sec2_parameter_estimation/example14.m
@@ -26,21 +26,46 @@ theta0 = zeros(2,1);
 % Controller
 K = [0.2, 0.32];
 
-%% Plot
-Theta = [
-    -2, 1;
-    -2, 0;
-    -2, -1;
-    1, 1;
-    1, 0;
-    1, -1
-    ]';
+%%
+S = sim("closed_loop_estimation.slx");
+
+LW = 2; % Line Width
 figure
+plot(S.theta.Time, S.theta.Data(:,1), 'b', 'LineWidth', LW)
+hold on
+plot([start_time stop_time], [a a], 'b--', 'LineWidth', LW)
+plot(S.theta.Time, S.theta.Data(:,2), 'r','LineWidth', LW)
+plot([start_time stop_time], [b b], 'r--', 'LineWidth', LW)
+hold off
+grid on
+legend({'$\hat{a}$', '$a$', '$\hat{b}$', '$b$'},'Interpreter','latex')
+xlabel('Sample Time')
+%% Plot
+LW = 2; % Line Width
+
+Theta = [
+    -1.75, 1;
+    -1.5, 0;
+    -1.25, -1;
+    0, 1.5;
+    0.25, 0.5;
+    0.5, -0.5
+    ]';
+
+b_hat = [-1, 1.5];
+a_hat = -K(1) * (b_hat - b) + a;
+
+figure
+%plot(a_hat, b_hat, 'k--', 'LineWidth', LW)
 hold on
 for i = 1:6
     seed_e = i;
     theta0 = Theta(:,i);
     S = sim("closed_loop_estimation.slx");
-    plot(S.theta.Data(:,1), S.theta.Data(:,2), 'b--')
-    plot(S.theta.Data(end,1), S.theta.Data(end,2), 'o')
+    plot(S.theta.Data(:,1), S.theta.Data(:,2), 'b', 'LineWidth', LW)
 end
+axis equal
+hold off
+grid on
+xlabel({'$\hat{a}$'},'Interpreter','latex')
+ylabel({'$\hat{b}$'},'Interpreter','latex')
